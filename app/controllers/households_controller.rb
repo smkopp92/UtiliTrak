a5ba775506a6008e3ce74ed1bd0ca335abb396e2
Sebastian <smkopp92@gmail.com>
2016-01-18 12:03:22 -0500

    add/show features for households

--- app/controllers/households_controller.rb
+++ app/controllers/households_controller.rb
@@ -1,4 +1,30 @@
 class HouseholdsController < ApplicationController
   def index
+    @user = current_user
+    @households = Household.where(user: @user)
+  end
+  def show
+
+  end
+  def new
+    @user = current_user
+    @household = Household.new
+  end
+  def create
+    @user = current_user
+    @household = Household.new(household_params)
+    @household.user = current_user
+    if @household.save
+      flash[:notice] = "Household Created Successfully"
+      redirect_to households_path
+    else
+      flash[:errors] = @household.errors.full_messages.join(". ")
+      render :new
+    end
+  end
+  private
+
+  def household_params
+    params.require(:household).permit(:address, :city, :state, :zip, :occupants)
   end
 end
---
+++ spec/features/04user_sees_and_creates_households_spec.rb
@@ -0,0 +1,52 @@
+# As an authenticated user
+# I want to see a list of my households
+# So that I can access specific information for each location
+# Acceptance Criteria
+# []The index page should have a list of households
+# []Each element should be a link to the show page
+#
+# As an authenticated user
+# I want to add a household
+# So that I can keep track of all my bills
+# Acceptance Criteria
+# []I must fill out all the information correctly
+# []I should be redirected to show page after submitting form
+# []Incorrect information should raise an error and refresh page
+require 'spec_helper'
+
+feature 'user sees a list of their households and adds households' do
+  before(:each) do
+    @user = FactoryGirl.create(:user)
+    @household = FactoryGirl.create(:household, user_id: @user.id)
+    visit new_user_session_path
+    fill_in 'Email', with: @user.email
+    fill_in 'Password', with: @user.password, match: :prefer_exact
+    click_on 'Log in'
+  end
+  scenario 'user visits household show page and sees a list their households' do
+    visit households_path
+    expect(page).to have_content(@household.address)
+  end
+
+  scenario 'user adds household' do
+    visit new_household_path
+    fill_in 'Address', with: '123 Main Street'
+    fill_in 'City', with: 'New York'
+    fill_in 'State', with: 'NY', match: :prefer_exact
+    fill_in 'Zip', with: '01234'
+    click_on 'Create Household'
+    expect(page).to have_content('123 Main Street')
+  end
+
+  scenario 'unauthenticated user adding household' do
+    click_link "Sign Out"
+    visit new_household_path
+    expect(page).to have_content('Need to be signed in to add a house!')
+  end
+
+  scenario 'unauthenticated user seeing households' do
+    click_link "Sign Out"
+    visit households_path
+    expect(page).to have_content('Need to be signed in to see your houses!')
+  end
+end
