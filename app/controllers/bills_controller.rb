d69a75b174d3209726e7614349435cc83284820a
Sebastian <smkopp92@gmail.com>
2016-01-21 11:27:46 -0500

    hound 4

--- app/controllers/bills_controller.rb
+++ app/controllers/bills_controller.rb
@@ -75,23 +75,23 @@ class BillsController < ApplicationController
         type: 'pie',
         name: 'Amount($)',
         data: d
-        }
+      }
       f.series(series)
       f.options[:title][:text] = text
       f.legend(layout: 'vertical', style: {
         left: 'auto', bottom: 'auto', right: '50px', top: '100px'
-        })
+      })
       f.plot_options(pie: {
         allowPointSelect: true,
         cursor: "pointer",
         dataLabels: {
           enabled: true,
           color: "black",
-          style:{
+          style: {
             font: "13px Trebuchet MS, Verdana, sans-serif"
-            }
           }
-        })
+        }
+      })
     end
   end
 
