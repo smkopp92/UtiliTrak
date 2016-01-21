ed68a0005b07101f16bc93c28408e419d90d59ce
Sebastian <smkopp92@gmail.com>
2016-01-21 11:20:04 -0500

    hound 1'

--- app/controllers/bills_controller.rb
+++ app/controllers/bills_controller.rb
@@ -3,7 +3,7 @@ class BillsController < ApplicationController
     @user = current_user
     @household = Household.find(params[:household_id])
     @bill = Bill.new
-    @kinds = ["Electric","Gas","Water"] # Gas, Water next
+    @kinds = ["Electric", "Gas", "Water"]
   end
 
   def create
@@ -11,7 +11,7 @@ class BillsController < ApplicationController
     @household = Household.find(params[:household_id])
     @bill = Bill.new(bill_params)
     @bill.household = @household
-    @kinds = ["Electric","Gas","Water"] # Gas, Water next
+    @kinds = ["Electric", "Gas", "Water"]
     if @bill.save
       @bill.update(date: "#{@bill.date.year}/#{@bill.date.month}/1")
       flash[:notice] = "Bill Created Successfully"
@@ -26,14 +26,14 @@ class BillsController < ApplicationController
     @user = current_user
     @household = Household.find(params[:household_id])
     @bill = Bill.find(params[:id])
-    @kinds = ["Electric","Gas","Water"] # Gas, Water next
+    @kinds = ["Electric", "Gas", "Water"]
   end
 
   def update
     @user = current_user
     @household = Household.find(params[:household_id])
     @bill = Bill.find(params[:id])
-    @kinds = ["Electric","Gas","Water"] # Gas, Water next
+    @kinds = ["Electric", "Gas", "Water"]
     if @bill.update(bill_params)
       @bill.update(date: "#{@bill.date.year}/#{@bill.date.month}/1")
       flash[:notice] = "Bill updated successfully"
@@ -69,26 +69,26 @@ class BillsController < ApplicationController
                               date: @bill.date, amount: 10000000)
     end
     @chart3 = LazyHighCharts::HighChart.new('pie') do |f|
-          f.chart({:defaultSeriesType=>"pie" , :margin=> [50, 200, 60, 170]} )
-          series = {
-                   :type=> 'pie',
-                   :name=> 'Amount($)',
-                   :data=> d
+      f.chart({:defaultSeriesType=>"pie" , :margin=> [50, 200, 60, 170]} )
+      series = {
+               :type=> 'pie',
+               :name=> 'Amount($)',
+               :data=> d
+      }
+      f.series(series)
+      f.options[:title][:text] = "Breakdown for #{@bill.date.month}/#{@bill.date.year}"
+      f.legend(layout: 'vertical', style: {left: 'auto', bottom: 'auto',right: '50px',top: '100px'})
+      f.plot_options(pie:{
+        allowPointSelect:true,
+        cursor:"pointer",
+        dataLabels:{
+          enabled:true,
+          color:"black",
+          style:{
+            font:"13px Trebuchet MS, Verdana, sans-serif"
           }
-          f.series(series)
-          f.options[:title][:text] = "Breakdown for #{@bill.date.month}/#{@bill.date.year}"
-          f.legend(:layout=> 'vertical',:style=> {:left=> 'auto', :bottom=> 'auto',:right=> '50px',:top=> '100px'})
-          f.plot_options(:pie=>{
-            :allowPointSelect=>true,
-            :cursor=>"pointer" ,
-            :dataLabels=>{
-              :enabled=>true,
-              :color=>"black",
-              :style=>{
-                :font=>"13px Trebuchet MS, Verdana, sans-serif"
-              }
-            }
-          })
+        }
+      })
     end
   end
 
