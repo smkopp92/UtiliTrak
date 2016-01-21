29d06e44f69bccf25562c045e77a7839082f95b8
Sebastian <smkopp92@gmail.com>
2016-01-21 11:25:40 -0500

    hound 3

--- app/controllers/bills_controller.rb
+++ app/controllers/bills_controller.rb
@@ -70,26 +70,28 @@ class BillsController < ApplicationController
     end
     text = "Breakdown for #{@bill.date.month}/#{@bill.date.year}"
     @chart3 = LazyHighCharts::HighChart.new('pie') do |f|
-      f.chart({ defaultSeriesType:"pie", margin: [50, 200, 60, 170] } )
+      f.chart({ defaultSeriesType: "pie", margin: [50, 200, 60, 170] } )
       series = {
         type: 'pie',
         name: 'Amount($)',
         data: d
-      }
+        }
       f.series(series)
       f.options[:title][:text] = text
-      f.legend(layout: 'vertical', style: { left: 'auto', bottom: 'auto', right: '50px', top: '100px' })
-      f.plot_options(pie:{
-        allowPointSelect:true,
-        cursor:"pointer",
-        dataLabels:{
-          enabled:true,
-          color:"black",
+      f.legend(layout: 'vertical', style: {
+        left: 'auto', bottom: 'auto', right: '50px', top: '100px'
+        })
+      f.plot_options(pie: {
+        allowPointSelect: true,
+        cursor: "pointer",
+        dataLabels: {
+          enabled: true,
+          color: "black",
           style:{
-            font:"13px Trebuchet MS, Verdana, sans-serif"
+            font: "13px Trebuchet MS, Verdana, sans-serif"
+            }
           }
-        }
-      })
+        })
     end
   end
 
